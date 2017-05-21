575b17bf424548aa40e6eae77272acd40fc6b9a3
Jordan Hudgens <jordan@devcamp.com>
2017-05-21 14:33:08 +0000

    Implemented custom scope

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |topic|
+  Topic.create!(title: "My Topic #{topic}")
+end  
+puts "3 topics created"
+  
 10.times do |blog|
   Blog.create!(title: "My blog post #{blog}", body: "Loving JavaScript
 We’ve had a stormy, perhaps even contentious, relationship with JavaScript over the years. But that time is past. JavaScript has improved immensely over the past few years, particularly with the advent of ES6, and with package and compilation tools like Yarn and webpack. Rails is embracing both of these solutions with open arms and letting whatever past water flow under the bridge.
-JavaScript and Ruby share a deep philosophical bond over language design, if not ecosystem management. Let’s focus on the aspects we have in common and help Rails programmers extract the best from JavaScript with the help of some key guiding conventions."
+JavaScript and Ruby share a deep philosophical bond over language design, if not ecosystem management. Let’s focus on the aspects we have in common and help Rails programmers extract the best from JavaScript with the help of some key guiding conventions.",
+topic_id: Topic.last.id
 )
 end
 puts "10 blogs created"
@@ -18,13 +25,24 @@ puts "10 blogs created"
 end
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
     body: "Action Mailer is modeled on Action Controller. It shares underpinnings through Abstract Controller, but it’s long been disadvantaged from its controller cousin in the way it can share logic between actions.", 
     main_image: "http://placehold.it/600x400", 
     thumb_image: "http://placehold.it/350x200"
     )
   end
-  puts "9 portfolio items created"
+  puts "8 portfolio items created"
+  
+  1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Action Mailer is modeled on Action Controller. It shares underpinnings through Abstract Controller, but it’s long been disadvantaged from its controller cousin in the way it can share logic between actions.", 
+    main_image: "http://placehold.it/600x400", 
+    thumb_image: "http://placehold.it/350x200"
+    )
+  end
+  puts "1 portfolio items created"

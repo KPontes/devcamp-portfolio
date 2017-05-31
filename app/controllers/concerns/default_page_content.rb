3b82b0981ab8309bb18d0b97ddd1bef02f025cd6
Jordan Hudgens <jordan@devcamp.com>
2017-05-31 16:05:44 +0000

    Added nav bar and def head content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,14 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+  
+  included do
+    before_filter :set_page_defaults
+  end
+  
+  def set_page_defaults
+    @page_title = "DevCamp Portfolio"
+    @seo_keywords = "Krishnan Pontes Portfolio"
+  end
+  
+
+end

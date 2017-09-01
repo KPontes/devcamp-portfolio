b2acb6fb45eabc09542e1c67e1281b2082aabc6c
Jordan Hudgens <jordan@devcamp.com>
2017-09-01 17:52:48 +0000

    Updated default page concern

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
   
   def set_page_defaults
-    @page_title = "DevCamp Portfolio"
+    @page_title = "KLP Presentation"
     @seo_keywords = "Krishnan Pontes Portfolio"
   end
   
